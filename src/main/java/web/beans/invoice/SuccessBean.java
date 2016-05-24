c0609396dfc438b4a50582c14a3c69c1581a093a
Hoeks, Maikel <maikelhoeks@hotmail.com>
2016-05-24 13:23:37 +0200

    added paypal payment

---
+++ src/main/java/web/beans/invoice/SuccessBean.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.beans.invoice;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import main.core.communication.Communicator;
+import main.domain.Invoice;
+import main.domain.enums.PaymentStatus;
+import org.codehaus.jettison.json.JSONException;
+import web.beans.UserInfoBean;
+import web.core.helpers.ContextHelper;
+import web.core.helpers.RedirectHelper;
+
+/**
+ *
+ * @author maikel
+ */
+@Named
+@ViewScoped
+public class SuccessBean implements Serializable {
+    
+    @Inject
+    private UserInfoBean userInfoBean;
+    private Long invoiceId;
+    private Invoice invoice;
+    
+    public void init() {
+        if (!ContextHelper.isAjaxRequest()) {
+            try {
+                invoice = Communicator.getInvoice(userInfoBean.getLoggedInUser().getId(), invoiceId);
+                invoice.setPaymentStatus(PaymentStatus.PAID);
+                Communicator.updateInvoice(userInfoBean.getLoggedInUser().getId(), invoice);
+                RedirectHelper.redirect("/pages/invoice/invoiceOverview.xhtml");
+            } catch (IOException | JSONException ex) {
+                Logger.getLogger(SuccessBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public Long getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(Long invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+    }
+    
+    
+}
